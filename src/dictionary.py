45ed97109ec9f966f7eb630c672ba00441388ab4
Hanif Carroll <HanifCarroll@gmail.com>
2019-12-09 14:58:49 -0500

    Change dictionary api

--- src/dictionary.py
+++ src/dictionary.py
@@ -1,12 +1,23 @@
-from PyDictionary import PyDictionary
+from config import DICTIONARY_API_KEY, DICTIONARY_APP_ID
+import json
+import requests
 
-dictionary = PyDictionary()
+BASE_URL = f'https://od-api.oxforddictionaries.com/api/v2/entries/en-us/'
 
 def getDefinition(word):
-    definition = dictionary.meaning(word)
-    result = ""
-    for key in definition:
-        for meaning in definition[key]:
-            result += f'{key} - {meaning}\n'
+    if not word:
+        return "Something went wrong."
+
+    url = BASE_URL + word.lower() + '?fields=definitions'
+    r = requests.get(url, headers = {"app_id": DICTIONARY_APP_ID, "app_key": DICTIONARY_API_KEY})
+    response = r.json()
+
+    result = ''
+    for res in response['results']:
+        for lexicalEntry in res['lexicalEntries']:
+            for entry in lexicalEntry['entries']:
+                for sense in entry['senses']:
+                    for definition in sense['definitions']:
+                        result += definition.capitalize() + '\n'
 
     return result
